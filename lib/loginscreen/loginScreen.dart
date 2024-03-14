8fa3bdc468495c03b72617c3eefc50a24a1a3bb1
sadeqfwade <sadeq202077@gmail.com>
2024-03-15 01:48:56 +0300

    sadeq

--- lib/loginscreen/loginScreen.dart
+++ lib/loginscreen/loginScreen.dart
@@ -1,13 +1,14 @@
 // ignore_for_file: unused_import
 
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_application_commerce/constent.dart';
+import 'package:flutter_application_commerce/loginscreen/second_screen.dart';
 import 'package:flutter_application_commerce/widget/custem_Button.dart';
 import 'package:flutter_application_commerce/widget/custem_Button_sochalmaide.dart';
 import 'package:flutter_application_commerce/widget/custem_text.dart';
 import 'package:flutter_application_commerce/widget/custem_text_form_field.dart';
+import 'package:get/get.dart';
+
 class loginScreen extends StatefulWidget {
   const loginScreen({super.key});
 
@@ -21,63 +22,109 @@ class _loginScreenState extends State<loginScreen> {
     return Scaffold(
       appBar: AppBar(
         elevation: 0.0,
-        backgroundColor: Colors.white,
+        backgroundColor: const Color.fromARGB(255, 241, 236, 236),
       ),
       body: Padding(
-        padding: const EdgeInsets.only(top: 50,right: 20,left: 20,),
-        child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        padding: const EdgeInsets.only(
+          top: 50,
+          right: 20,
+          left: 20,
+        ),
+        child: SingleChildScrollView(
+          child: Column(
             children: [
-              custem_text(
-                text: "Welcome,",fontSize: 30,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  custem_text(
+                    text: "Welcome,",
+                    fontSize: 30,
+                  ),
+                  custem_text(
+                    text: "Sign Up",
+                    color: primaryColor,
+                    fontSize: 18,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 3,
               ),
               custem_text(
-                text: "Sign Up",
-              color: primaryColor,
-              fontSize: 18,
+                text: 'Sign in to couninue',
+                fontSize: 14,
+                color: Colors.grey,
               ),
-              SizedBox(height: 10,),
-
-             
-             
+              const SizedBox(
+                height: 40,
+              ),
+              custem_text_form_field(
+                text: 'Email',
+                hint: 'iamdevid@gmail.com',
+                onSave: () {},
+                validator: () {},
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              custem_text_form_field(
+                text: 'password',
+                hint: '*************',
+                onSave: () {},
+                validator: () {},
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              custem_text(
+                text: 'Forgot Password?',
+                fontSize: 14,
+                alignment: Alignment.topRight,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              custem_Button(
+                onPressed: () {
+                  Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => const SignUp()
+    ),
+  
+                  );
+                  Get.to(SignUp()
+                  );
+                },
+                text: 'SING IN',
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              custem_text(
+                text: '-OR-',
+                alignment: Alignment.center,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              custem_Button_shchalmaide(
+                  text: 'Sign ln  with Fecbook',
+                  imagename: 'asset/facbook.jpg',
+                  onPressed: () {}),
+              const SizedBox(
+                height: 10,
+              ),
+              custem_Button_shchalmaide(
+                  text: 'Sign ln  with Google',
+                  imagename: 'asset/google.jpg',
+                  onPressed: () {}),
             ],
-            
           ),
-          SizedBox(height: 3,),
-           custem_text(text: 'Sign in to couninue',
-              fontSize: 14,
-              color: Colors.grey,
-              ),
-              SizedBox(height: 40,),
-            custem_text_form_field(text: 'Email', hint: 'iamdevid@gmail.com', onSave:(){} , validator: (){},),
-            SizedBox(height: 20,),
-              
-              custem_text_form_field(text: 'password', hint: '*************', onSave:(){} , validator: (){},),
-              SizedBox(height: 20,),
-              custem_text(text: 'Forgot Password?',
-              fontSize: 14,
-             alignment: Alignment.topRight,
-              ),
-               SizedBox(height: 20,),
-             custem_Button(text:'SING IN' , onPressed: (){},),
-             SizedBox(height: 20,),
-             custem_text(text: '-OR-',
-             alignment: Alignment.center,
-             ),
-             SizedBox(height: 10,),
-    custem_Button_shchalmaide( text: 'Sign ln  with Fecbook' , imagename:'asset/facbook.jpg',onPressed: (){} ),
-                SizedBox(height: 10,),
-    custem_Button_shchalmaide( text: 'Sign ln  with Google' , imagename:'asset/google.jpg',onPressed: (){} ),
-
-
-        
-        ],
         ),
       ),
-        
-      
     );
   }
 }
